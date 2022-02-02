c3a0500d359aabc254956cc5a22ad0fda52acb73
Bladerox <pa.sa95@gmx.de>
2022-02-02 14:58:00 +0100

    added cronos testnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -10,6 +11,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const getHDWallet = () => {
+  const { MNEMONIC, PRIVATE_KEY } = process.env;
+  if (MNEMONIC && MNEMONIC !== "") {
+    return {
+      mnemonic: MNEMONIC,
+    }
+  }
+  if (PRIVATE_KEY && PRIVATE_KEY !== "") {
+    return [PRIVATE_KEY]
+  }
+  throw Error("Private Key Not Set! Please set up .env");
+}
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +31,24 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "testnetCronos",
+  networks:{
+    hardhat:{
+
+    },
+    devnode: {
+      url:"http://127.0.0.1:8545",
+    },
+    testnetCronos:{
+      url:"http://dragon-alpha:8545",
+      chainId: 338,
+      accounts: getHDWallet()
+    }
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
